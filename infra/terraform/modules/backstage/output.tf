85d8e4070654237002c854a2b4b9592d917021c4
Rafael Tomelin <rafael.tomelin@gmail.com>
2024-09-11 08:41:08 -0300

    update

--- infra/terraform/modules/backstage/output.tf
+++
@@ -1,9 +0,0 @@
-# output "entity_json" {
-#   value       = local.object
-#   description = "The JSON representation of the Backstage Entity"
-# }
-
-output "status_response" {
-  value       = data.http.send_data.status_code
-  description = "The JSON representation of the Backstage Entity"
-}
