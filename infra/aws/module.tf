4c392a9b02a39e8be7fa95b2b7f42d2a9533e425
Rafael Tomelin <rafael.tomelin@gmail.com>
2024-10-12 01:35:35 -0300

    update terraform files

--- infra/aws/module.tf
+++ infra/aws/module.tf
@@ -1,7 +1,7 @@
 
 
 module "ascode" {
-  source = "git::https://github.com/kubediscovery/platform-infrastructure.git//modules/aws/eks_cluster?ref=aws"
+  source = "git::https://github.com/kubediscovery/platform-infrastructure.git//modules/platform/labs/aws?ref=aws"
 
   project_name = local.yaml_configs.name
   cidr_block   = local.yaml_configs.cidr
--- infra/azure/module.tf
+++ infra/azure/module.tf
@@ -1,7 +1,7 @@
 
 
 module "ascode" {
-  source = "git::https://github.com/kubediscovery/platform-infrastructure.git//modules/azure/aks_cluster?ref=aws"
+  source = "git::https://github.com/kubediscovery/platform-infrastructure.git//modules/platform/labs/azure?ref=aws"
 
   project_name = local.yaml_configs.name
   cidr_block   = local.yaml_configs.cidr
